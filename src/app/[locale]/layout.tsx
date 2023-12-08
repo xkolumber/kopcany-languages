87ec7dd0c6b30c2fa404a25eaaccedd89afd71cf
Lubos Kolumber <lubosk158@gmail.com>
2023-12-08 01:44:50 +0100

    for

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,15 +1,21 @@
 import { notFound } from "next/navigation";
+import { ReactNode } from "react";
 
-// Can be imported from a shared config
 const locales = ["sk", "cz", "en"];
 
-export default function LocaleLayout({ children, params: { locale } }) {
-  // Validate that the incoming `locale` parameter is valid
-  if (!locales.includes(locale as any)) notFound();
+interface LocaleLayoutProps {
+  children: ReactNode;
+  locale: string;
+}
+
+const LocaleLayout = ({ children, locale }: LocaleLayoutProps) => {
+  if (!locales.includes(locale)) notFound();
 
   return (
     <html lang={locale}>
       <body>{children}</body>
     </html>
   );
-}
+};
+
+export default LocaleLayout;
