e58dc07a4982384bf29fb62aa381fb93da112124
Lubos Kolumber <lubosk158@gmail.com>
2023-12-12 23:42:34 +0100

    Four pictures transition at homepage

--- src/components/FourPictures.tsx
+++ src/components/FourPictures.tsx
@@ -1,6 +1,6 @@
 "use client";
+
 import React, { useEffect, useState } from "react";
-import Image from "next/image";
 
 const FourPictures = () => {
   const images = [
@@ -10,22 +10,38 @@ const FourPictures = () => {
     "/kopcany_uvod.jpg",
   ];
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
+  const [scale, setScale] = useState(1);
 
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => (prevIndex + 1) % images.length);
+      setScale(1);
+      setTimeout(() => setScale(1.05), 100);
     }, 5000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [images.length]);
+
   return (
-    <Image
-      src={images[currentImageIndex]}
-      alt={`Image ${currentImageIndex + 1}`}
-      fill={true}
-      style={{ objectFit: "cover" }}
-      className="bg_image"
-    />
+    <div className="imageWrapper">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className="imageContainer"
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            opacity: index === currentImageIndex ? 1 : 0,
+            transition: "opacity 1s ease-in, transform 0.5s ease-in-out",
+            transform: `scale(${index === currentImageIndex ? scale : 1})`,
+            transformOrigin: "center center",
+          }}
+        >
+          <img src={image} alt={`Image ${index + 1}`} className="bgImage" />
+        </div>
+      ))}
+    </div>
   );
 };
 
