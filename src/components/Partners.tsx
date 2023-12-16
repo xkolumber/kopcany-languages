e300a959516aef6a2ad3dcff874e85f770c32212
Lubos Kolumber <lubosk158@gmail.com>
2023-12-16 17:08:58 +0100

    Partners css

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -8,25 +8,32 @@ const Partners = () => {
       <p className="text-black">Toto sú naši partneri</p>
       <div className="partners">
         <Image
-          src="/partner1.jpg"
-          width={600}
-          height={100}
+          src="/partner0.svg"
+          width={100}
+          height={50}
           alt="foto1"
-          className="partner_img"
+          className="partner_img_first"
+        />
+        <Image
+          src="/partner1__.svg"
+          width={100}
+          height={50}
+          alt="foto1"
+          className="partner_img_second"
         />
         <Image
           src="/partner2.jpg"
-          width={200}
-          height={100}
+          width={100}
+          height={50}
           alt="foto1"
-          className="partner_img"
+          className="partner_img_third"
         />
         <Image
-          src="/partner3.jpg"
-          width={200}
-          height={100}
+          src="/partner3.svg"
+          width={100}
+          height={50}
           alt="foto1"
-          className="partner_img"
+          className="partner_img_fourth"
         />
       </div>
     </>
