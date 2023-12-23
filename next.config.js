80b1a2e3cafb6f3ad141ac163d5c4981b243882d
xkolumber <lubosk158@gmail.com>
2023-12-23 16:29:08 +0100

    Static sitemap

--- next.config.js
+++ next.config.js
@@ -4,16 +4,7 @@ const withNextIntl = require('next-intl/plugin')();
 const nextConfig = {
     images:{
         domains: ['cdn.sanity.io'],
-    },
-    async rewrites() {
-        return [
-          {
-            source: '/sitemap.xml',
-            destination: '/api/sitemap',
-          },
-        ];
-      },
-    
+    },    
 };
 
 module.exports = withNextIntl(nextConfig);
