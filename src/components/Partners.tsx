e3ec293c26df7ffda0a7d3b727d479a5b888fef7
xkolumber <lubosk158@gmail.com>
2024-01-19 11:46:57 +0100

    bigger logo's

--- src/components/Partners.tsx
+++ src/components/Partners.tsx
@@ -3,56 +3,41 @@ import { useTranslations } from "next-intl";
 const Partners = () => {
   const t = useTranslations("partners");
 
-
   return (
     <>
       <h2 className="text-black partners_title">{t("title")}</h2>
       <p className="text-black"> {t("description")}</p>
       <div className="partners">
-        
-      <img src="/all_partners.jpg" alt="All Partners images" className="partners_img_phone" /> 
+        <img
+          src="/all_partners.jpg"
+          alt="All Partners images"
+          className="partners_img_phone"
+        />
 
-
-     
-          <img  src="/partner0.svg" alt="Parner 1 logo"  className="partner_img_first"/>
-          <img  src="/partner1.svg" alt="Parner 1 logo"  className="partner_img_second"/>
-          <img  src="/partner2.jpg" alt="Parner 1 logo"  className="partner_img_third"/>
-          <img  src="/partner3.svg" alt="Parner 1 logo"  className="partner_img_fourth"/> 
-    
-       
-        {/* /}
-        {/* <Image
-          src="/partner0.svg"
-          width={0}
-          height={0}
-          sizes="100vw"
-          alt="Partner 1 logo"
-          className="partner_img_first"
-        />
-        <Image
-          src="/partner1.svg"
-          width={0}
-          height={0}
-          sizes="100vw"
-          alt="Partner 2 logo"
-          className="partner_img_second"
-        />
-        <Image
-          src="/partner2.jpg"
-          width={0}
-          height={0}
-          sizes="100vw"
-          alt="Partner 3 logo"
-          className="partner_img_third"
-        />
-        <Image
-          src="/partner3.svg"
-          width={0}
-          height={0}
-          sizes="100vw"
-          alt="Partner 4 logo"
-          className="partner_img_fourth"
-        /> */}
+        <div className="partners_row">
+          <img
+            src="/partner0.svg"
+            alt="Parner 1 logo"
+            className="partner_img_first"
+          />
+          <img
+            src="/partner1.svg"
+            alt="Parner 1 logo"
+            className="partner_img_second"
+          />
+        </div>
+        <div className="partners_row2">
+          <img
+            src="/partner2.jpg"
+            alt="Parner 1 logo"
+            className="partner_img_third"
+          />
+          <img
+            src="/partner3.svg"
+            alt="Parner 1 logo"
+            className="partner_img_fourth"
+          />
+        </div>
       </div>
     </>
   );
